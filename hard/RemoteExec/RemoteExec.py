08c3c517a6b725065648a6457535b05144629d6d
axeoman <axeoman@gmail.com>
2014-03-26 15:46:52 +0400

    Without config and timeout

--- hard/RemoteExec/RemoteExec.py
+++ hard/RemoteExec/RemoteExec.py
@@ -33,21 +33,21 @@ class Handler(asyncore.dispatcher_with_send):
 	
 class Server(asyncore.dispatcher):
 
-    def __init__(self, host, port):
-        asyncore.dispatcher.__init__(self)
-        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.set_reuse_addr()
-        self.bind((host, port))
-        self.listen(5)
+	def __init__(self, host, port):
+		asyncore.dispatcher.__init__(self)
+		self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.set_reuse_addr()
+		self.bind((host, port))
+		self.listen(5)
 
-    def handle_accept(self):
-        pair = self.accept()
-        if pair is None:
-            pass
-        else:
-            sock, addr = pair
-            print 'Incoming connection from %s' % repr(addr)
-            handler = Handler(sock)
+	def handle_accept(self):
+		pair = self.accept()
+		if pair is None:
+			pass
+		else:
+			sock, addr = pair
+			print 'Incoming connection from %s' % repr(addr)
+			handler = Handler(sock)
 
 server = Server('localhost', 8080)
 asyncore.loop()
